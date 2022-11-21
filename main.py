2668038a59b1e6875d1d7f00ec3dfa17677e16a2
xovae <89520672+xovae@users.noreply.github.com>
2022-11-21 15:07:27 +1300

    fix to core transistor logic, updated log.txt

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 import machine
 import utime
-import machine 
 
 led = machine.Pin('LED', machine.Pin.OUT)
 led.high()
 
-#leftMotor = machine.Pin(16, machine.Pin.OUT) # bottom pin
 class motor:
-    def forward(this):
-        this.posTrans2.low()
-        this.negTrans2.low()
-        this.posTrans1.high()
-        this.negTrans1.high()
-
-    def backwards(this):
-        this.posTrans1.low()
-        this.negTrans1.low()
-        this.negTrans2.high()
-        this.posTrans2.high()
-
     def __init__(this, posTrans1, posTrans2, negTrans1, negTrans2):
         this.posTrans1 = machine.Pin(posTrans1, machine.Pin.OUT)
         this.posTrans2 = machine.Pin(posTrans2, machine.Pin.OUT)
         this.negTrans1 = machine.Pin(negTrans1, machine.Pin.OUT)
         this.negTrans2 = machine.Pin(negTrans2, machine.Pin.OUT)
+        
+def forward(this):
+        this.posTrans2.low()
+        this.negTrans2.low()
+        this.posTrans1.high()
+        this.negTrans1.high()
 
-leftMotor = motor(15, 14, 13, 12)
-rightMotor = motor(16, 17, 18, 19)
+def backwards(this):
+        this.posTrans1.low()
+        this.negTrans1.low()
+        this.negTrans2.high()
+        this.posTrans2.high()
+
+leftMotor = motor(15, 14, 12, 13)
+rightMotor = motor(16, 17, 19, 18)
 
 while True:
-    leftMotor.forward()
-    rightMotor.forward()
+    forward(leftMotor)
+    forward(rightMotor)
+    # leftMotor.forward()
+    # rightMotor.forward()
